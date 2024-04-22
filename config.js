1ca1f3fbddafbbb6b978c801e29a32965415e746
Oiva <hyyppaoiva@gmail.com>
2024-04-22 17:18:31 +0300

    removed api leak

--- config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    jellyfinServerUrl: 'https://media.kickfresh.shop',
-    apiToken: '3417133c480a4969a08cfcad58ea3545',
-    bookLibraryId: '4e985111ed7f570b595204d82adb02f3',
-    userId: 'ca5244a6c24d431783a7d7a993a73dda'
-};
